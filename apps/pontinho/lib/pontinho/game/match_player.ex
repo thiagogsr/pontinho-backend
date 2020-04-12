65c0a97d2cb276d6bd6ffb180da6d1cab57daf4a
Thiago Guimaraes <thiagogsr@gmail.com>
2020-04-12 19:41:27 -0300

    MatchPlayer schema and migration

---
+++ apps/pontinho/lib/pontinho/game/match_player.ex
@@ -0,0 +1,20 @@
+defmodule Pontinho.MatchPlayer do
+  @moduledoc """
+  Match player schema
+  """
+
+  use Ecto.Schema
+
+  alias Pontinho.{Match, Player}
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  schema "match_players" do
+    field :cards, {:array, :map}
+    belongs_to :match, Match
+    belongs_to :player, Player
+
+    timestamps()
+  end
+end
---
+++ apps/pontinho/priv/repo/migrations/20200412223715_create_match_players.exs
@@ -0,0 +1,16 @@
+defmodule Pontinho.Repo.Migrations.CreateMatchPlayers do
+  use Ecto.Migration
+
+  def change do
+    create table(:match_players, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :cards, {:array, :map}, null: false
+      add :match_id, references(:matches, on_delete: :nothing, type: :binary_id), null: false
+      add :player_id, references(:players, on_delete: :nothing, type: :binary_id), null: false
+
+      timestamps()
+    end
+
+    create unique_index(:match_players, [:match_id, :player_id])
+  end
+end
