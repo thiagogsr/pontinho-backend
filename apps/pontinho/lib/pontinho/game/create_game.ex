6ee3717b0be8886c2f2f961a9b27d3553f4eb066
Thiago Guimaraes <thiagogsr@gmail.com>
2020-04-12 18:42:39 -0300

    Create a new game

---
+++ apps/pontinho/lib/pontinho/game/create_game.ex
@@ -0,0 +1,19 @@
+defmodule Pontinho.CreateGame do
+  @moduledoc """
+  Create a new game
+  """
+
+  alias Pontinho.{Game, Repo}
+
+  import Ecto.Changeset
+
+  @spec run(list(pos_integer)) :: {:ok, %Game{}} | {:error, %Ecto.Changeset{}}
+  def run(betting_table) do
+    %Game{}
+    |> change()
+    |> put_change(:betting_table, betting_table)
+    |> validate_length(:betting_table, min: 5, max: 11)
+    |> put_change(:status, "waiting_players")
+    |> Repo.insert()
+  end
+end
---
+++ apps/pontinho/test/pontinho/game/create_game_test.exs
@@ -0,0 +1,20 @@
+defmodule Pontinho.CreateGameTest do
+  use Pontinho.DataCase, async: true
+
+  alias Pontinho.{CreateGame, Game}
+
+  test "returns the created game" do
+    betting_table = [50, 100, 200, 400, 800]
+
+    assert {:ok, %Game{} = game} = CreateGame.run(betting_table)
+    assert game.betting_table == betting_table
+    assert game.status == "waiting_players"
+  end
+
+  test "returns a changeset error" do
+    betting_table = [50, 100, 200]
+
+    assert {:error, %Ecto.Changeset{} = changeset} = CreateGame.run(betting_table)
+    assert %{betting_table: ["should have at least 5 item(s)"]} = errors_on(changeset)
+  end
+end
