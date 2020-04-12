7d24ceee5962ea7436222281c71d0c10a5607fbd
Thiago Guimaraes <thiagogsr@gmail.com>
2020-04-12 17:54:47 -0300

    Game schema and migration

---
+++ apps/pontinho/lib/pontinho/game/game.ex
@@ -0,0 +1,17 @@
+defmodule Pontinho.Game do
+  @moduledoc """
+  Game schema
+  """
+
+  use Ecto.Schema
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  schema "games" do
+    field :betting_table, {:array, :integer}
+    field :status, :string
+
+    timestamps()
+  end
+end
---
+++ apps/pontinho/priv/repo/migrations/20200412204907_create_games.exs
@@ -0,0 +1,14 @@
+defmodule Pontinho.Repo.Migrations.CreateGames do
+  use Ecto.Migration
+
+  def change do
+    create table(:games, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :status, :string, null: false
+      add :betting_table, {:array, :integer}, null: false
+
+      timestamps()
+    end
+
+  end
+end
