1b51b4e60ccbec5f8c1d352bf78e0bfb42441c7b
Thiago Guimaraes <thiagogsr@gmail.com>
2020-06-20 17:12:32 -0300

    Promote a new host when the current host abandons the game

--- apps/pontinho/lib/pontinho/game/abandon_game.ex
+++ apps/pontinho/lib/pontinho/game/abandon_game.ex
@@ -4,6 +4,7 @@ defmodule Pontinho.AbandonGame do
   """
 
   import Ecto.Changeset
+  import Ecto.Query, only: [from: 2]
 
   alias Pontinho.{Player, Repo}
 
@@ -11,11 +12,44 @@ defmodule Pontinho.AbandonGame do
 
   @spec run(%Player{}) :: {:ok, %Player{}} | {:error, %Ecto.Changeset{}}
   def run(%Player{} = player) do
+    Repo.transaction(fn ->
+      with {:ok, updated_player} <- update_player(player),
+           {:ok, _new_host} <- choose_new_host(player) do
+        updated_player
+      else
+        {:error, changeset} -> Repo.rollback(changeset)
+      end
+    end)
+  end
+
+  defp update_player(player) do
     player
     |> change()
     |> put_change(:broke_times, calculate_broke_times(player))
     |> put_change(:playing, false)
+    |> put_change(:host, false)
     |> Repo.update()
+  end
+
+  defp choose_new_host(%Player{host: true, id: player_id, game_id: game_id}) do
+    query =
+      from(
+        p in Player,
+        where: p.game_id == ^game_id,
+        where: p.id != ^player_id,
+        where: p.playing,
+        order_by: p.inserted_at,
+        limit: 1
+      )
+
+    query
+    |> Repo.one()
+    |> change(%{host: true})
+    |> Repo.update()
+  end
+
+  defp choose_new_host(%Player{host: false} = player) do
+    {:ok, player}
   end
 
   defp calculate_broke_times(%Player{broke_times: broke_times})
--- apps/pontinho/test/pontinho/game/abandon_game_test.exs
+++ apps/pontinho/test/pontinho/game/abandon_game_test.exs
@@ -3,18 +3,27 @@ defmodule Pontinho.AbandonGameTest do
 
   import Pontinho.Factory
 
-  alias Pontinho.{AbandonGame, Player}
+  alias Pontinho.{AbandonGame, Player, Repo}
 
   test "updates the player broke times to 4 when it has less than 4 brokes" do
-    player = insert(:player, broke_times: 0, playing: true)
+    player = insert(:player, broke_times: 0, playing: true, host: false)
     assert {:ok, %Player{broke_times: 4, playing: false}} = AbandonGame.run(player)
   end
 
+  test "chooses a new host when the player is the host" do
+    game = insert(:game)
+    player1 = insert(:player, game: game, broke_times: 0, playing: true, host: true)
+    player2 = insert(:player, game: game, playing: true, host: false)
+
+    assert {:ok, %Player{broke_times: 4, playing: false, host: false}} = AbandonGame.run(player1)
+    assert %Player{host: true} = Repo.get(Player, player2.id)
+  end
+
   test "does not change the player broke times when it has greater or equals to 4 brokes" do
-    player = insert(:player, broke_times: 4, playing: true)
+    player = insert(:player, broke_times: 4, playing: true, host: false)
     assert {:ok, %Player{broke_times: 4, playing: false}} = AbandonGame.run(player)
 
-    player = insert(:player, broke_times: 5, playing: true)
+    player = insert(:player, broke_times: 5, playing: true, host: false)
     assert {:ok, %Player{broke_times: 5, playing: false}} = AbandonGame.run(player)
   end
 end
