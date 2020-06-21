0320f7056a086c58081dd5f305f1fae88e63652c
Thiago Guimaraes <thiagogsr@gmail.com>
2020-06-20 22:34:57 -0300

    Fix sequence suits validation

--- apps/pontinho/lib/pontinho/collection/validate_sequence.ex
+++ apps/pontinho/lib/pontinho/collection/validate_sequence.ex
@@ -17,24 +17,39 @@ defmodule Pontinho.Collection.ValidateSequence do
   """
   @spec run(list(map), map | nil) :: :ok_with_joker | :ok_without_joker | :error
   def run(cards, joker \\ nil) do
-    [first_card | tail_cards] = Enum.map(cards, &convert_card/1)
-    joker_card = convert_card(joker)
-
-    case validate(first_card, tail_cards, joker_card) do
-      :error -> :error
-      %{with_joker: true} -> :ok_with_joker
-      %{with_joker: false} -> :ok_without_joker
+    with :ok <- validate_suits(cards, joker) do
+      case validate_values(cards, joker) do
+        :error -> :error
+        %{with_joker: true} -> :ok_with_joker
+        %{with_joker: false} -> :ok_without_joker
+      end
     end
   end
 
-  defp convert_card(card) when is_nil(card), do: nil
-
-  defp convert_card(card) do
-    {Deck.value_index(card["value"]), card["suit"]}
+  defp validate_suits(cards, joker) when is_nil(joker) do
+    case Enum.uniq_by(cards, & &1["suit"]) do
+      suits when length(suits) == 1 -> :ok
+      _ -> :error
+    end
   end
 
-  defp validate(first_card, tail_cards, joker) do
+  defp validate_suits(cards, joker) do
+    suits =
+      cards
+      |> Enum.reject(&(&1["value"] == joker["value"] && &1["suit"] == joker["suit"]))
+      |> Enum.uniq_by(& &1["suit"])
+
+    if length(suits) == 1 do
+      :ok
+    else
+      :error
+    end
+  end
+
+  defp validate_values(cards, joker) do
+    [first_card | tail_cards] = Enum.map(cards, &convert_card/1)
     {first_index, _} = first_card
+    joker_card = convert_card(joker)
 
     Enum.reduce_while(tail_cards, %{card: first_card, with_joker: false}, fn card, acc ->
       {index, suit} = card
@@ -44,10 +59,16 @@ defmodule Pontinho.Collection.ValidateSequence do
         king_ace_two?(index, past_index, first_index) -> {:halt, :error}
         ace_after_king?(index, past_index) -> {:cont, %{card: card, with_joker: with_joker}}
         greater_index?(index, past_index) -> {:cont, %{card: card, with_joker: with_joker}}
-        joker?(card, joker) -> {:cont, %{card: {past_index + 1, suit}, with_joker: true}}
+        joker?(card, joker_card) -> {:cont, %{card: {past_index + 1, suit}, with_joker: true}}
         true -> {:halt, :error}
       end
     end)
+  end
+
+  defp convert_card(card) when is_nil(card), do: nil
+
+  defp convert_card(card) do
+    {Deck.value_index(card["value"]), card["suit"]}
   end
 
   defp king_ace_two?(current_index, past_index, first_index) do
--- apps/pontinho/test/pontinho/collection/collection_test.exs
+++ apps/pontinho/test/pontinho/collection/collection_test.exs
@@ -11,9 +11,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "A", "suit" => "hearts", "deck" => 1}
-      beat = false
 
-      assert {:error, "at least 3 cards"} = Collection.validate(cards, joker, beat)
+      assert {:error, "at least 3 cards"} = Collection.validate(cards, joker, true)
+      assert {:error, "at least 3 cards"} = Collection.validate(cards, joker, false)
     end
 
     test "returns ok when it is a sequence without joker" do
@@ -24,9 +24,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
 
-      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, true)
+      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, false)
     end
 
     test "returns ok when it is a sequence with a joker out of corners" do
@@ -37,9 +37,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "10", "suit" => "spades", "deck" => 2}
-      beat = false
 
-      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, true)
+      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, false)
     end
 
     test "returns ok when it is a sequence with a joker in the corner acting as a regular card" do
@@ -50,12 +50,12 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "A", "suit" => "hearts", "deck" => 1}
-      beat = false
 
-      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, true)
+      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, false)
     end
 
-    test "returns ok when it is a sequence with a joker in the corner and player beats" do
+    test "validates sequence with a joker in the corner" do
       cards = [
         %{"value" => "A", "suit" => "hearts", "deck" => 1},
         %{"value" => "2", "suit" => "hearts", "deck" => 1},
@@ -63,9 +63,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "10", "suit" => "spades", "deck" => 1}
-      beat = true
 
-      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, true)
+      assert {:error, "joker cannot be in the corner"} = Collection.validate(cards, joker, false)
     end
 
     test "returns ok when it is a sequence with ACE after KING" do
@@ -76,9 +76,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
 
-      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, true)
+      assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, false)
     end
 
     test "returns ok when it is a sequence with joker and other card with the same value" do
@@ -94,20 +94,7 @@ defmodule Pontinho.CollectionTest do
       assert {:ok, %{type: "sequence"}} = Collection.validate(cards, joker, false)
     end
 
-    test "returns error when it is a sequence with a joker in the corner and player does not beat" do
-      cards = [
-        %{"value" => "A", "suit" => "hearts", "deck" => 1},
-        %{"value" => "2", "suit" => "hearts", "deck" => 1},
-        %{"value" => "10", "suit" => "spades", "deck" => 2}
-      ]
-
-      joker = %{"value" => "10", "suit" => "spades", "deck" => 1}
-      beat = false
-
-      assert {:error, "joker cannot be in the corner"} = Collection.validate(cards, joker, beat)
-    end
-
-    test "returns error when there are duplicated cards in a trio" do
+    test "returns error when there are duplicated cards" do
       cards = [
         %{"value" => "A", "suit" => "hearts", "deck" => 1},
         %{"value" => "A", "suit" => "hearts", "deck" => 1},
@@ -115,9 +102,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
 
-      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, beat)
+      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, true)
+      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, false)
     end
 
     test "returns error when the sequence is unordered" do
@@ -128,9 +115,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
 
-      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, beat)
+      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, true)
+      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, false)
     end
 
     test "returns error when the sequence is K, A, 2" do
@@ -141,9 +128,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
 
-      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, beat)
+      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, true)
+      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, false)
     end
 
     test "returns error when the sequence is unordered with joker" do
@@ -154,9 +141,22 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
 
-      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, beat)
+      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, true)
+      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, false)
+    end
+
+    test "returns error when the sequence has different suits" do
+      cards = [
+        %{"value" => "4", "suit" => "hearts", "deck" => 1},
+        %{"value" => "5", "suit" => "spades", "deck" => 1},
+        %{"value" => "6", "suit" => "spades", "deck" => 1}
+      ]
+
+      joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
+
+      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, true)
+      assert {:error, "invalid sequence"} = Collection.validate(cards, joker, false)
     end
 
     test "returns ok then it is a trio without a joker" do
@@ -167,9 +167,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
 
-      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, true)
+      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, false)
     end
 
     test "returns ok then it is a trio with joker acting as a regular card" do
@@ -180,9 +180,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "3", "suit" => "hearts", "deck" => 1}
-      beat = false
 
-      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, true)
+      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, false)
     end
 
     test "returns ok then it is a trio without a joker and four cards" do
@@ -194,12 +194,12 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
 
-      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, true)
+      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, false)
     end
 
-    test "returns ok then it is a trio with a joker and player beats" do
+    test "validates trio with a joker" do
       cards = [
         %{"value" => "3", "suit" => "hearts", "deck" => 1},
         %{"value" => "K", "suit" => "spades", "deck" => 1},
@@ -207,25 +207,12 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 2}
-      beat = true
 
-      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, true)
+      assert {:error, "invalid trio"} = Collection.validate(cards, joker, false)
     end
 
-    test "returns ok then it is a trio with a joker acting as a regular card and player beats" do
-      cards = [
-        %{"value" => "3", "suit" => "hearts", "deck" => 1},
-        %{"value" => "3", "suit" => "spades", "deck" => 1},
-        %{"value" => "3", "suit" => "diamonds", "deck" => 1}
-      ]
-
-      joker = %{"value" => "3", "suit" => "spades", "deck" => 2}
-      beat = true
-
-      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, beat)
-    end
-
-    test "returns ok when there are four cards and one of them is the joker and player beats" do
+    test "validates trio with four cards and one of them is the joker" do
       cards = [
         %{"value" => "3", "suit" => "hearts", "deck" => 1},
         %{"value" => "3", "suit" => "spades", "deck" => 1},
@@ -234,12 +221,12 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 2}
-      beat = true
 
-      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, true)
+      assert {:error, "invalid trio"} = Collection.validate(cards, joker, false)
     end
 
-    test "returns ok when there are duplicated cards for a trio with joker and player beats" do
+    test "validates trio with duplicated cards and joker" do
       cards = [
         %{"value" => "3", "suit" => "spades", "deck" => 1},
         %{"value" => "3", "suit" => "spades", "deck" => 1},
@@ -247,22 +234,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "3", "suit" => "spades", "deck" => 1}
-      beat = true
 
-      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, beat)
-    end
-
-    test "returns error when there are duplicated cards for a trio with joker and player does not beat" do
-      cards = [
-        %{"value" => "3", "suit" => "spades", "deck" => 1},
-        %{"value" => "3", "suit" => "spades", "deck" => 1},
-        %{"value" => "3", "suit" => "diamonds", "deck" => 1}
-      ]
-
-      joker = %{"value" => "3", "suit" => "spades", "deck" => 1}
-      beat = false
-
-      assert {:error, "invalid trio"} = Collection.validate(cards, joker, beat)
+      assert {:ok, %{type: "trio"}} = Collection.validate(cards, joker, true)
+      assert {:error, "invalid trio"} = Collection.validate(cards, joker, false)
     end
 
     test "returns error when it is a trio with duplicated cards" do
@@ -273,22 +247,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
 
-      assert {:error, "invalid trio"} = Collection.validate(cards, joker, beat)
-    end
-
-    test "returns error when it is a trio with a joker and player does not beat" do
-      cards = [
-        %{"value" => "3", "suit" => "hearts", "deck" => 1},
-        %{"value" => "K", "suit" => "spades", "deck" => 1},
-        %{"value" => "3", "suit" => "diamonds", "deck" => 1}
-      ]
-
-      joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
-
-      assert {:error, "invalid trio"} = Collection.validate(cards, joker, beat)
+      assert {:error, "invalid trio"} = Collection.validate(cards, joker, true)
+      assert {:error, "invalid trio"} = Collection.validate(cards, joker, false)
     end
 
     test "returns error when there are four suits" do
@@ -300,9 +261,9 @@ defmodule Pontinho.CollectionTest do
       ]
 
       joker = %{"value" => "K", "suit" => "spades", "deck" => 1}
-      beat = false
 
-      assert {:error, "invalid trio"} = Collection.validate(cards, joker, beat)
+      assert {:error, "invalid trio"} = Collection.validate(cards, joker, true)
+      assert {:error, "invalid trio"} = Collection.validate(cards, joker, false)
     end
   end
 end
