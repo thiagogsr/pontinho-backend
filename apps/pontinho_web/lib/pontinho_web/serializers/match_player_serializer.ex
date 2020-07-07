a8153ae2856f44f407a019a48cf002a556cdce32
Thiago Guimaraes <thiagogsr@gmail.com>
2020-07-06 23:47:30 -0300

    Match player abilities

--- apps/pontinho_web/lib/pontinho_web/serializers/match_player_serializer.ex
+++ apps/pontinho_web/lib/pontinho_web/serializers/match_player_serializer.ex
@@ -4,28 +4,118 @@ defmodule PontinhoWeb.MatchPlayerSerializer do
   """
 
   @spec serialize(map) :: map
-  def serialize(%{match_player: match_player, previous_event: previous_event})
+  def serialize(%{
+        match_player: match_player,
+        round_match_player: round_match_player,
+        previous_event: previous_event
+      })
       when is_nil(previous_event) do
+    my_time = match_player.id == round_match_player.id
+
     %{
       match_player_id: match_player.id,
       match_player_hand: match_player.hand,
-      false_beat: match_player.false_beat,
-      asked_beat: false,
-      bought_first_card: false,
-      taked_discard_pile: false,
-      taked_card: nil
+      taked_card: nil,
+      abilities: %{
+        add_card_to_collection: false,
+        answer_first_card: false,
+        ask_beat: false,
+        beat: false,
+        buy: false,
+        buy_first_card: my_time,
+        discard: false,
+        drop_collection: false,
+        false_beat: false,
+        replace_joker: false,
+        take_discard_pile: false
+      }
     }
   end
 
-  def serialize(%{match_player: match_player, previous_event: previous_event}) do
+  def serialize(%{
+        match_player: match_player,
+        round_match_player: round_match_player,
+        previous_event: previous_event
+      }) do
+    my_time = match_player.id == round_match_player.id
+
     %{
       match_player_id: match_player.id,
       match_player_hand: match_player.hand,
-      false_beat: match_player.false_beat,
-      asked_beat: previous_event.type == "ASK_BEAT",
-      bought_first_card: previous_event.type == "BUY_FIRST_CARD",
-      taked_discard_pile: previous_event.type == "TAKE_DISCARD_PILE",
-      taked_card: previous_event.taked_card
+      taked_card: previous_event.taked_card,
+      abilities: %{
+        add_card_to_collection:
+          add_card_to_collection_ability(my_time, match_player, previous_event),
+        answer_first_card: answer_first_card_ability(my_time, previous_event),
+        ask_beat: ask_beat_ability(my_time, match_player, previous_event),
+        beat: beat_ability(my_time, match_player),
+        buy: buy_ability(my_time, previous_event),
+        buy_first_card: false,
+        discard: discard_ability(my_time, match_player, previous_event),
+        drop_collection: drop_collection_ability(my_time, match_player, previous_event),
+        false_beat: false_beat_abiluty(my_time, match_player, previous_event),
+        replace_joker: replace_joker_ability(my_time, match_player, previous_event),
+        take_discard_pile: take_discard_pile_ability(my_time, match_player, previous_event)
+      }
     }
+  end
+
+  defp add_card_to_collection_ability(my_time, match_player, previous_event) do
+    my_time &&
+      length(match_player.hand) > 0 &&
+      previous_event.type not in ["BUY_FIRST_CARD", "REJECT_FIRST_CARD"]
+  end
+
+  defp answer_first_card_ability(my_time, previous_event) do
+    my_time && previous_event.type == "BUY_FIRST_CARD"
+  end
+
+  defp ask_beat_ability(my_time, match_player, previous_event) do
+    previous_event.type not in ["BUY_FIRST_CARD", "REJECT_FIRST_CARD"] &&
+      !match_player.asked_beat &&
+      (my_time ||
+         (!match_player.false_beat &&
+            previous_event.type == "DISCARD" &&
+            length(match_player.hand) > 1))
+  end
+
+  defp beat_ability(my_time, match_player) do
+    my_time && length(match_player.hand) == 0
+  end
+
+  defp buy_ability(my_time, previous_event) do
+    my_time && previous_event.type in ["DISCARD", "REJECT_FIRST_CARD"]
+  end
+
+  defp discard_ability(my_time, match_player, previous_event) do
+    my_time &&
+      !match_player.asked_beat &&
+      length(match_player.hand) > 1 &&
+      previous_event.type not in [
+        "DISCARD",
+        "TAKE_DISCARD_PILE",
+        "BUY_FIRST_CARD",
+        "REJECT_FIRST_CARD"
+      ]
+  end
+
+  defp drop_collection_ability(my_time, match_player, previous_event) do
+    my_time &&
+      length(match_player.hand) >= 3 &&
+      previous_event.type not in ["BUY_FIRST_CARD", "REJECT_FIRST_CARD"]
+  end
+
+  defp false_beat_abiluty(my_time, match_player, previous_event) do
+    my_time && match_player.asked_beat && previous_event.type == "ASK_BEAT"
+  end
+
+  defp replace_joker_ability(my_time, match_player, previous_event) do
+    my_time &&
+      length(match_player.hand) > 0 &&
+      previous_event.type not in ["BUY_FIRST_CARD", "REJECT_FIRST_CARD"]
+  end
+
+  defp take_discard_pile_ability(my_time, match_player, previous_event) do
+    my_time && match_player.id != previous_event.match_player_id
   end
 end
--- apps/pontinho_web/test/pontinho_web/serializers/match_player_serializer_test.exs
+++ apps/pontinho_web/test/pontinho_web/serializers/match_player_serializer_test.exs
@@ -1,102 +1,419 @@
 defmodule PontinhoWeb.MatchPlayerSerializerTest do
-  use ExUnit.Case, async: true
+  use Pontinho.DataCase, async: true
 
   import Pontinho.Factory
 
   alias PontinhoWeb.MatchPlayerSerializer
 
   describe "serialize/1" do
-    test "returns a serialized match player with previous_event's type equals to ASK_BEAT" do
-      match_player = build(:match_player)
-      match_event = build(:match_event, type: "ASK_BEAT")
+    test "returns the serialized match player when there is no previous event" do
+      match_player = insert(:match_player, asked_beat: false)
 
       assert %{
                match_player_id: _,
-               match_player_hand: match_player_hand,
-               asked_beat: true,
-               bought_first_card: false,
-               taked_discard_pile: false,
-               taked_card: nil
+               match_player_hand: _,
+               taked_card: nil,
+               abilities: %{
+                 add_card_to_collection: false,
+                 answer_first_card: false,
+                 ask_beat: false,
+                 beat: false,
+                 buy: false,
+                 buy_first_card: true,
+                 discard: false,
+                 drop_collection: false,
+                 false_beat: false,
+                 replace_joker: false,
+                 take_discard_pile: false
+               }
              } =
                MatchPlayerSerializer.serialize(%{
                  match_player: match_player,
-                 previous_event: match_event
+                 previous_event: nil,
+                 round_match_player: match_player
                })
-
-      assert is_list(match_player_hand)
-      assert length(match_player_hand) == 9
     end
 
-    test "returns a serialized match player with previous_event's type equals to BUY_FIRST_CARD" do
-      match_player = build(:match_player)
-
-      match_event =
-        build(:match_event,
-          type: "BUY_FIRST_CARD",
-          taked_card: %{"value" => "10", "suit" => "hearts", "deck" => 1}
+    test "returns the serialized match player when it is my time" do
+      match_player =
+        insert(:match_player,
+          hand: [
+            %{"value" => "10", "suit" => "hearts", "deck" => 1},
+            %{"value" => "J", "suit" => "hearts", "deck" => 1},
+            %{"value" => "Q", "suit" => "hearts", "deck" => 1}
+          ]
         )
 
+      match_event = insert(:match_event, match_player: match_player, type: "BUY")
+
       assert %{
                match_player_id: _,
-               match_player_hand: match_player_hand,
-               asked_beat: false,
-               bought_first_card: true,
-               taked_discard_pile: false,
-               taked_card: %{"value" => "10", "suit" => "hearts", "deck" => 1}
+               match_player_hand: _,
+               taked_card: nil,
+               abilities: %{
+                 add_card_to_collection: true,
+                 answer_first_card: false,
+                 ask_beat: true,
+                 beat: false,
+                 buy: false,
+                 buy_first_card: false,
+                 discard: true,
+                 drop_collection: true,
+                 false_beat: false,
+                 replace_joker: true,
+                 take_discard_pile: false
+               }
              } =
                MatchPlayerSerializer.serialize(%{
                  match_player: match_player,
-                 previous_event: match_event
+                 previous_event: match_event,
+                 round_match_player: match_player
                })
-
-      assert is_list(match_player_hand)
-      assert length(match_player_hand) == 9
     end
 
-    test "returns a serialized match player with previous_event's type equals to TAKE_DISCARD_PILE" do
-      match_player = build(:match_player)
-
-      match_event =
-        build(:match_event,
-          type: "TAKE_DISCARD_PILE",
-          taked_card: %{"value" => "10", "suit" => "hearts", "deck" => 1}
+    test "returns the serialized match player when it is the not my time and the previous event is DISCARD" do
+      match_player1 =
+        insert(:match_player,
+          false_beat: false,
+          hand: [
+            %{"value" => "10", "suit" => "hearts", "deck" => 1},
+            %{"value" => "J", "suit" => "hearts", "deck" => 1}
+          ]
         )
 
-      assert %{
-               match_player_id: _,
-               match_player_hand: match_player_hand,
-               asked_beat: false,
-               bought_first_card: false,
-               taked_discard_pile: true,
-               taked_card: %{"value" => "10", "suit" => "hearts", "deck" => 1}
-             } =
-               MatchPlayerSerializer.serialize(%{
-                 match_player: match_player,
-                 previous_event: match_event
-               })
-
-      assert is_list(match_player_hand)
-      assert length(match_player_hand) == 9
-    end
-
-    test "returns a serialized match player without previous_event" do
-      match_player = build(:match_player)
+      match_player2 = insert(:match_player)
+      match_event = insert(:match_event, match_player: match_player1, type: "DISCARD")
 
       assert %{
                match_player_id: _,
-               match_player_hand: match_player_hand,
-               asked_beat: false,
-               bought_first_card: false,
-               taked_discard_pile: false,
-               taked_card: nil
+               match_player_hand: _,
+               taked_card: nil,
+               abilities: %{
+                 add_card_to_collection: false,
+                 answer_first_card: false,
+                 ask_beat: true,
+                 beat: false,
+                 buy: false,
+                 buy_first_card: false,
+                 discard: false,
+                 drop_collection: false,
+                 false_beat: false,
+                 replace_joker: false,
+                 take_discard_pile: false
+               }
+             } =
+               MatchPlayerSerializer.serialize(%{
+                 match_player: match_player1,
+                 previous_event: match_event,
+                 round_match_player: match_player2
+               })
+    end
+
+    test "returns the serialized match player when it is the not my time, the previous event is DISCARD but match player has only one card" do
+      match_player1 =
+        insert(:match_player,
+          false_beat: false,
+          hand: [%{"value" => "10", "suit" => "hearts", "deck" => 1}]
+        )
+
+      match_player2 = insert(:match_player)
+      match_event = insert(:match_event, match_player: match_player1, type: "DISCARD")
+
+      assert %{
+               match_player_id: _,
+               match_player_hand: _,
+               taked_card: nil,
+               abilities: %{
+                 add_card_to_collection: false,
+                 answer_first_card: false,
+                 ask_beat: false,
+                 beat: false,
+                 buy: false,
+                 buy_first_card: false,
+                 discard: false,
+                 drop_collection: false,
+                 false_beat: false,
+                 replace_joker: false,
+                 take_discard_pile: false
+               }
+             } =
+               MatchPlayerSerializer.serialize(%{
+                 match_player: match_player1,
+                 previous_event: match_event,
+                 round_match_player: match_player2
+               })
+    end
+
+    test "returns the serialized match player when it is the not my time and false beat" do
+      match_player1 = insert(:match_player, false_beat: true)
+      match_player2 = insert(:match_player)
+      match_event = insert(:match_event, match_player: match_player1, type: "DISCARD")
+
+      assert %{
+               match_player_id: _,
+               match_player_hand: _,
+               taked_card: nil,
+               abilities: %{
+                 add_card_to_collection: false,
+                 answer_first_card: false,
+                 ask_beat: false,
+                 beat: false,
+                 buy: false,
+                 buy_first_card: false,
+                 discard: false,
+                 drop_collection: false,
+                 false_beat: false,
+                 replace_joker: false,
+                 take_discard_pile: false
+               }
+             } =
+               MatchPlayerSerializer.serialize(%{
+                 match_player: match_player1,
+                 previous_event: match_event,
+                 round_match_player: match_player2
+               })
+    end
+
+    test "returns the serialized match player when it is the not my time and the previous event is not DISCARD" do
+      match_player1 = insert(:match_player, false_beat: false)
+      match_player2 = insert(:match_player)
+      match_event = insert(:match_event, match_player: match_player1, type: "BUY")
+
+      assert %{
+               match_player_id: _,
+               match_player_hand: _,
+               taked_card: nil,
+               abilities: %{
+                 add_card_to_collection: false,
+                 answer_first_card: false,
+                 ask_beat: false,
+                 beat: false,
+                 buy: false,
+                 buy_first_card: false,
+                 discard: false,
+                 drop_collection: false,
+                 false_beat: false,
+                 replace_joker: false,
+                 take_discard_pile: false
+               }
+             } =
+               MatchPlayerSerializer.serialize(%{
+                 match_player: match_player1,
+                 previous_event: match_event,
+                 round_match_player: match_player2
+               })
+    end
+
+    test "returns the serialized match player when it asked beat" do
+      match_player = insert(:match_player, asked_beat: true, false_beat: false)
+      match_event = insert(:match_event, type: "DISCARD")
+
+      assert %{
+               match_player_id: _,
+               match_player_hand: _,
+               taked_card: nil,
+               abilities: %{
+                 add_card_to_collection: true,
+                 answer_first_card: false,
+                 ask_beat: false,
+                 beat: false,
+                 buy: true,
+                 buy_first_card: false,
+                 discard: false,
+                 drop_collection: true,
+                 false_beat: false,
+                 replace_joker: true,
+                 take_discard_pile: true
+               }
              } =
                MatchPlayerSerializer.serialize(%{
                  match_player: match_player,
-                 previous_event: nil
+                 previous_event: match_event,
+                 round_match_player: match_player
                })
-
-      assert is_list(match_player_hand)
-      assert length(match_player_hand) == 9
     end
+
+    test "returns the serialized match player when it taked the first card" do
+      match_player = insert(:match_player)
+      match_event = insert(:match_event, match_player: match_player, type: "BUY_FIRST_CARD")
+
+      assert %{
+               match_player_id: _,
+               match_player_hand: _,
+               taked_card: nil,
+               abilities: %{
+                 add_card_to_collection: false,
+                 answer_first_card: true,
+                 ask_beat: false,
+                 beat: false,
+                 buy: false,
+                 buy_first_card: false,
+                 discard: false,
+                 drop_collection: false,
+                 false_beat: false,
+                 replace_joker: false,
+                 take_discard_pile: false
+               }
+             } =
+               MatchPlayerSerializer.serialize(%{
+                 match_player: match_player,
+                 previous_event: match_event,
+                 round_match_player: match_player
+               })
+    end
+
+    test "returns a serialized match match when match player rejects the first card" do
+      match_player = insert(:match_player)
+      match_event = insert(:match_event, match_player: match_player, type: "REJECT_FIRST_CARD")
+
+      assert %{
+               match_player_id: _,
+               match_player_hand: _,
+               taked_card: nil,
+               abilities: %{
+                 add_card_to_collection: false,
+                 answer_first_card: false,
+                 ask_beat: false,
+                 beat: false,
+                 buy: true,
+                 buy_first_card: false,
+                 discard: false,
+                 drop_collection: false,
+                 false_beat: false,
+                 replace_joker: false,
+                 take_discard_pile: false
+               }
+             } =
+               MatchPlayerSerializer.serialize(%{
+                 match_player: match_player,
+                 previous_event: match_event,
+                 round_match_player: match_player
+               })
+    end
+
+    test "returns a serialized match player when its hand is empty" do
+      match_player = insert(:match_player, hand: [])
+
+      match_event =
+        insert(:match_event, match_player: match_player, type: "ADD_CARD_TO_COLLECTION")
+
+      assert %{
+               match_player_id: _,
+               match_player_hand: _,
+               taked_card: nil,
+               abilities: %{
+                 add_card_to_collection: false,
+                 answer_first_card: false,
+                 ask_beat: true,
+                 beat: true,
+                 buy: false,
+                 buy_first_card: false,
+                 discard: false,
+                 drop_collection: false,
+                 false_beat: false,
+                 replace_joker: false,
+                 take_discard_pile: false
+               }
+             } =
+               MatchPlayerSerializer.serialize(%{
+                 match_player: match_player,
+                 previous_event: match_event,
+                 round_match_player: match_player
+               })
+    end
+
+    # test "returns a serialized match player with previous_event's type equals to ASK_BEAT" do
+    #   match_player = build(:match_player)
+    #   match_event = build(:match_event, type: "ASK_BEAT")
+
+    #   assert %{
+    #            match_player_id: _,
+    #            match_player_hand: match_player_hand,
+    #            asked_beat: true,
+    #            bought_first_card: false,
+    #            taked_discard_pile: false,
+    #            taked_card: nil
+    #          } =
+    #            MatchPlayerSerializer.serialize(%{
+    #              match_player: match_player,
+    #              previous_event: match_event,
+    #              round_match_player_id: round_match_player_id
+    #            })
+
+    #   assert is_list(match_player_hand)
+    #   assert length(match_player_hand) == 9
+    # end
+
+    # test "returns a serialized match player with previous_event's type equals to BUY_FIRST_CARD" do
+    #   match_player = build(:match_player)
+
+    #   match_event =
+    #     build(:match_event,
+    #       type: "BUY_FIRST_CARD",
+    #       taked_card: %{"value" => "10", "suit" => "hearts", "deck" => 1}
+    #     )
+
+    #   assert %{
+    #            match_player_id: _,
+    #            match_player_hand: match_player_hand,
+    #            asked_beat: false,
+    #            bought_first_card: true,
+    #            taked_discard_pile: false,
+    #            taked_card: %{"value" => "10", "suit" => "hearts", "deck" => 1}
+    #          } =
+    #            MatchPlayerSerializer.serialize(%{
+    #              match_player: match_player,
+    #              previous_event: match_event
+    #            })
+
+    #   assert is_list(match_player_hand)
+    #   assert length(match_player_hand) == 9
+    # end
+
+    # test "returns a serialized match player with previous_event's type equals to TAKE_DISCARD_PILE" do
+    #   match_player = build(:match_player)
+
+    #   match_event =
+    #     build(:match_event,
+    #       type: "TAKE_DISCARD_PILE",
+    #       taked_card: %{"value" => "10", "suit" => "hearts", "deck" => 1}
+    #     )
+
+    #   assert %{
+    #            match_player_id: _,
+    #            match_player_hand: match_player_hand,
+    #            asked_beat: false,
+    #            bought_first_card: false,
+    #            taked_discard_pile: true,
+    #            taked_card: %{"value" => "10", "suit" => "hearts", "deck" => 1}
+    #          } =
+    #            MatchPlayerSerializer.serialize(%{
+    #              match_player: match_player,
+    #              previous_event: match_event
+    #            })
+
+    #   assert is_list(match_player_hand)
+    #   assert length(match_player_hand) == 9
+    # end
+
+    # test "returns a serialized match player without previous_event" do
+    #   match_player = build(:match_player)
+
+    #   assert %{
+    #            match_player_id: _,
+    #            match_player_hand: match_player_hand,
+    #            asked_beat: false,
+    #            bought_first_card: false,
+    #            taked_discard_pile: false,
+    #            taked_card: nil
+    #          } =
+    #            MatchPlayerSerializer.serialize(%{
+    #              match_player: match_player,
+    #              previous_event: nil
+    #            })
+
+    #   assert is_list(match_player_hand)
+    #   assert length(match_player_hand) == 9
+    # end
   end
 end
